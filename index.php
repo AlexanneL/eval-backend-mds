e2c4251a7c3a92a3348ca87c8e8f261d1356b0f3
AlexanneL <alexannelemaile@gmail.com>
2020-06-26 09:30:11 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>Bienvenue</title></head>
+    <body>
+        <h1>mon site internet</h1>
+        <h2>Inscription</h2>
+        <form name="inscription" method="post" action="saisie.php">
+            <!--nom de mon formulaire--> 
+            Entrez votre pseudo : <input type="text" alname="pseudo"/> <br/>
+            Entrez votre ville : <input type="text" alname="ville"/><br/>
+            <input type="submit" name="valider" value="OK"/>
+        </form>
+    </body>
+</html>
