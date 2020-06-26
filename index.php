f139db765b7af85d16617173221953bb70f41c54
AlexanneL <62385932+AlexanneL@users.noreply.github.com>
2020-06-26 11:03:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,15 @@
         <form name="inscription" method="post" action="saisie.php">
             <!--nom de mon formulaire--> 
             Entrez votre pseudo : <input type="text" alname="pseudo"/> <br/>
-            Entrez votre ville : <input type="text" alname="ville"/><br/>
+            Entrez votre ville : <input type="text" alnville="ville"/><br/>
+            Entrez votre numero : <input type="text" alnum="numero"/><br/>
+            Entrez votre email : <input type="text" almail="email"/><br/>
             <input type="submit" name="valider" value="OK"/>
+             <!--echo permet de collecter les données issues du formulaire-->
+            <?php echo "alname".$_GET[="pseudo"]."/> <br/>";
+            ?php echo "alnville".$_GET[="ville"]."/> <br/>";
+            ?php echo "alnum".$_GET[="numero"]."/> <br/>";
+            ?php echo "almail".$_GET[="email"]."/> <br/>";
         </form>
     </body>
 </html>
